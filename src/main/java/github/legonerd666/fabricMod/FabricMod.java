1a817a84d2bc961220c7e2520007d6af3e90697b
onlydragon0403 <brunogendron6827@gmail.com>
2023-05-07 12:11:53 -0500

    Added textures to block of ruby

--- src/main/java/github/legonerd666/fabricMod/FabricMod.java
+++ src/main/java/github/legonerd666/fabricMod/FabricMod.java
@@ -1,15 +1,17 @@
 package github.onlydragon0403.fabricMod;
 
 import github.onlydragon0403.fabricMod.registry.ModItems;
+import github.onlydragon0403.fabricMod.registry.ModBlocks;
 import net.fabricmc.api.ModInitializer;
 
 public class FabricMod implements ModInitializer {
 
-    @Override
-    public void onInitialize() {
-        System.out.println("I am running!");
-        ModItems.registerItems();
-        System.out.println("Registered Items.");
-    }
-    
+  @Override
+  public void onInitialize() {
+    System.out.println("I am running!");
+    ModBlocks.registerBlocks();
+    System.out.println("Registered Blocks.");
+    ModItems.registerItems();
+    System.out.println("Registered Items.");
+  }
 }
